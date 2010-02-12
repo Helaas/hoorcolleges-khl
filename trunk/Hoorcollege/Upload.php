71bb9f125990cc7b629f7dbdbe26a8a61f901e39
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-12 13:13:36 +0000

    Gebruiker geeftt vak+onderwerp op, indien nodig worden directories aangemaakt en files erin opgeslagen
    git-svn-id: svn://localhost/repo@8 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/Upload.php
+++ trunk/Hoorcollege/Upload.php
@@ -6,10 +6,24 @@ $testinhoud = "";
 
 
 
-$target_path = "TestBibliotheek/";
+ // if (is_dir("TestBibliotheek/".$_POST['vak']."/") && is_dir("TestBibliotheek/".$_POST['vak']."/".$_POST['onderwerp']."/")){
+   //   }
+
+    if (!is_dir("TestBibliotheek/".$_POST['vak']."/")){
+        mkdir("TestBibliotheek/".$_POST['vak'], 0777);
+    }
+
+        if (!is_dir("TestBibliotheek/".$_POST['vak']."/".$_POST['onderwerp']."/")){
+        mkdir("TestBibliotheek/".$_POST['vak']."/".$_POST['onderwerp'], 0777);
+    }
+
+
+
+$target_path = "TestBibliotheek/".$_POST['vak']."/".$_POST['onderwerp']."/";
+
 
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
-
+ 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
    $testinhoud="Het bestand ".'"'.  basename( $_FILES['uploadedfile']['name']).'"'." werd geüpload!";
 } else{
