4e50cd5e21111cbb710c4d72165d4aae0a0c33ce
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-12 21:41:09 +0000

    git-svn-id: svn://localhost/repo@24 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/includes/functions.php
+++ trunk/Hoorcollege/includes/functions.php
@@ -1,10 +1,10 @@
 <?php
     include_once('kern.php');
-    global $db ;
 
 
     //methode om na te gaan of een gebruiker met deze email al bestaat
     function bestaatEmail($email) {
+        global $db;
         $resultaat = $db->Execute("SELECT COUNT( DISTINCT email ) AS aantal
                                    FROM `hoorcollege_gebruiker` WHERE email = '$email'");
 
