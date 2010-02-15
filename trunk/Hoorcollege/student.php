647162c4b90a24947a8a6192b32d20194905241e
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-15 13:40:52 +0000

    Student krijgt overzicht van nog te bekijken hoorcolleges
    git-svn-id: svn://localhost/repo@49 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/student.php
+++ trunk/Hoorcollege/student.php
@@ -7,6 +7,15 @@ $TBS = new clsTinyButStrong;
 
 if(isset ($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() == 1){
     $config["pagina"] = "./student/student.html";
+    $TBS->LoadTemplate('./html/template.html') ;
+    $gebruikerID = $_SESSION['gebruiker']->getIdGebruiker();
+    //tabel aanmaken voor overzicht hoorcolleges
+    $query = 'SELECT * FROM hoorcollege_hoorcollege WHERE idHoorcollege IN (
+              SELECT Hoorcollege_idHoorcollege FROM hoorcollege_onderwerphoorcollege WHERE Onderwerp_Vak_idVak IN(
+              SELECT Vak_idVak FROM hoorcollege_gebruiker_volgt_vak WHERE Gebruiker_idGebruiker = '.$gebruikerID.' ))';
+
+    $TBS->MergeBlock('blk1', $db, $query);
+
 }else if(!isset ($_SESSION['gebruiker'])){
     header("location: login.php");
 }else{
@@ -14,7 +23,7 @@ if(isset ($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() == 1){
 }
 
 
-$TBS->LoadTemplate('./html/template.html') ;
+
 $TBS->Show() ;
 
 ?>
