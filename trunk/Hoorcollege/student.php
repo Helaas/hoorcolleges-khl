4efa3bc61cbad9c96e58342d13c0e5c8cd8d7fe3
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-15 18:35:39 +0000

    git-svn-id: svn://localhost/repo@60 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/student.php
+++ trunk/Hoorcollege/student.php
@@ -9,13 +9,16 @@ if(isset ($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() == 1){
     $config["pagina"] = "./student/student.html";
     $TBS->LoadTemplate('./html/template.html') ;
     $gebruikerID = $_SESSION['gebruiker']->getIdGebruiker();
-    //tabel aanmaken voor overzicht hoorcolleges
+
+    //Query voor overzicht hoorcolleges
     $query = 'SELECT * FROM hoorcollege_hoorcollege WHERE idHoorcollege IN (
               SELECT Hoorcollege_idHoorcollege FROM hoorcollege_onderwerphoorcollege WHERE Onderwerp_Vak_idVak IN(
               SELECT Vak_idVak FROM hoorcollege_gebruiker_volgt_vak WHERE Gebruiker_idGebruiker = '.$gebruikerID.' ))';
 
+    //Tabel nog te bekijken hoorcolleges opvullen (nog aan te passen)
     $TBS->MergeBlock('blk1', $db, $query);
 
+    //Tabel reeds bekeken hoorcolleges opvullen (nog aan te passen)
     $TBS->MergeBlock('blk2', $db, $query);
 
 }else if(!isset ($_SESSION['gebruiker'])){
