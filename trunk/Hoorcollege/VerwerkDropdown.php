895f82519d00ae62cf8de1d8203e648880f49314
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-16 18:06:03 +0000

    Eindelijk dependent dropdowns werkend gekregen, via xmlhttp request+xml en vervolgens via javascript een dropdown  invullen, het zat ver weg die javascript/ajaxx :/
    Kan gebruikt worden voor browsen van vakken/onderwerpen van een gebruiker, ben van plan zo de hoorcolleges te tonen,  zo blijft het overzichtelijk. Ook bij het toevoegen van onderwerpen/hoorcolleges later hoop ik dit te kunnen gebruiken, wel nog een probleem ivm een alert die blijkbaar vereist is, ga er ff een mail over sturen aangezien ik ni weet of iemand de subversion updates leest :p
    
    git-svn-id: svn://localhost/repo@65 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/VerwerkDropdown.php
+++ trunk/Hoorcollege/VerwerkDropdown.php
@@ -1,4 +1,27 @@
 <?php
-//Query om alle onderwerpen van een vak te vinden, zet dan om in XML, laad in naar js en vul dropdown2 ermee
+    include_once('./includes/kern.php');
+
+    header("Content-type: text/xml");
+    //gegevens uit de db halen
+    $result = $db->Execute("select * from hoorcollege_onderwerp where vak_idVak=".$_GET["gevraagdVak"]);
+
+    //xml file aanmaken
+$xml_file  = "<?xml version=\"1.0\"?>";
+$xml_file .= "<root>";
+
+
+    while (!$result->EOF) {
+      $xml_file .= "<Onderwerp>".$result->fields["naam"]."</Onderwerp>";
+      $xml_file .= "<Id>".$result->fields["idOnderwerp"]."</Id>";
+
+        $result->MoveNext();
+    }
+
+$xml_file .= "</root>";
+
+echo $xml_file;
+
+
+
 
 ?>
--- trunk/Hoorcollege/html/lector/Functies.js
+++ trunk/Hoorcollege/html/lector/Functies.js
@@ -1,16 +1,50 @@
-
-
 function autoSubmit(form)
 {
 
 
-  var vak=form.vak.options[form.vak.options.selectedIndex].value;
+  var vakid=form.vak.options[form.vak.options.selectedIndex].value;
   xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vak, true);
+  xmlhttp.onreadystatechange=Verwerk;
+  //xmlhttp request om via php een xml pagina aan te maken op basis van het meegegoven vakid
+  xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vakid, true);
+  xmlhttp.send(null);
 
-   alert(vak);
+   alert(xmlhttp.responseXML);
+
+  
+
+
+               //haal optie uit de XML en voeg toe aan dropdown
+                var elems = xmlhttp.responseXML.getElementsByTagName("Onderwerp");
+ 		var size = elems.length;
+                form.onderwerp.options[0]=null;
+                for(i = 0; i < size; i++){
+
+                 //onderwerp = naam van het onderwerp, id = id van het onderwerp, gebruiker krijgt de naam te zien, de var die doorgegoven word in 'onchange' is de id
+                 var inh=xmlhttp.responseXML.getElementsByTagName('Onderwerp')[i].firstChild.data;
+                 var id=xmlhttp.responseXML.getElementsByTagName('Id')[i].firstChild.data;
+                 form.onderwerp.options.add( new Option(inh,id));
+
+            }
 
 
    
+}
+
+function autoSubmit2(form)
+{
+
+
+  var Onderwerp=form.onderwerp.options[form.onderwerp.options.selectedIndex].value;
+  alert(Onderwerp);
+
+
+
+}
+function Verwerk()
+{
+if(xmlhttp.readyState == 4 && xmlHttp.status == 200)
+{
+}
 }
 
