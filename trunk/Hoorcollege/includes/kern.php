a17a3a90864ada6815ce7a4e6799675c89e6b751
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-16 23:49:03 +0000

    Gebruik maken van centrale MySQl server
    git-svn-id: svn://localhost/repo@66 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/includes/kern.php
+++ trunk/Hoorcollege/includes/kern.php
@@ -23,7 +23,11 @@
 
     // Verbinden
 
-    $db->Connect("localhost", $config["bdgebruiker"], $config["dbwachtwoord"], $config["dbnaam"]);
+    //$db->Connect("localhost", $config["bdgebruiker"], $config["dbwachtwoord"], $config["dbnaam"]);
+
+    //Eens die amazon dinges proberen
+    $db->Connect("ec2-79-125-51-239.eu-west-1.compute.amazonaws.com:3700","wortel", "nutella", "hoorcolleges");
+
 
 
     ## Template en het scheiden van HTML en 
