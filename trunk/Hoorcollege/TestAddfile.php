bad314e5b28ee7ac99643ec27df11efdc2d04f25
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 14:59:45 +0000

    Wijzigingen
    git-svn-id: svn://localhost/repo@76 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/TestAddfile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once('./includes/gebruiker.class.php');
-    $gebruiker = new Gebruiker('2', 'Laeremans', 'Tom', 'z', 'x', '40');
-
-    session_start();
-
-    $_SESSION['gebruiker'] = $gebruiker;
-    header('location: Addfile.php')
-?>
