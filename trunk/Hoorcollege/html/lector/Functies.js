bf0a49e4affa35c98fdcb49b133058c91e789d1d
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 14:19:30 +0000

    Dependent drop down lists, never again >>
    git-svn-id: svn://localhost/repo@74 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/html/lector/Functies.js
+++ trunk/Hoorcollege/html/lector/Functies.js
@@ -3,18 +3,16 @@ function autoSubmit(form)
 
 
   var vakid=form.vak.options[form.vak.options.selectedIndex].value;
+
   xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange=Verwerk;
   //xmlhttp request om via php een xml pagina aan te maken op basis van het meegegoven vakid
-  xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vakid, true);
+  xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vakid, false);
   xmlhttp.send(null);
 
-   alert(xmlhttp.responseXML);
+ //alert(xmlhttp.responseXML);
 
-  
-
-
-               //haal optie uit de XML en voeg toe aan dropdown
+                //haal optie uit de XML en voeg toe aan dropdown
                 var elems = xmlhttp.responseXML.getElementsByTagName("Onderwerp");
  		var size = elems.length;
                 form.onderwerp.options[0]=null;
@@ -25,10 +23,10 @@ function autoSubmit(form)
                  var id=xmlhttp.responseXML.getElementsByTagName('Id')[i].firstChild.data;
                  form.onderwerp.options.add( new Option(inh,id));
 
-            }
 
 
-   
+
+}
 }
 
 function autoSubmit2(form)
@@ -43,8 +41,20 @@ function autoSubmit2(form)
 }
 function Verwerk()
 {
+
 if(xmlhttp.readyState == 4 && xmlHttp.status == 200)
 {
-}
-}
+    alert('reached')
+               //haal optie uit de XML en voeg toe aan dropdown
+                var elems = xmlhttp.responseXML.getElementsByTagName("Onderwerp");
+ 		var size = elems.length;
+                form.onderwerp.options[0]=null;
+                for(i = 0; i < size; i++){
 
+                 //onderwerp = naam van het onderwerp, id = id van het onderwerp, gebruiker krijgt de naam te zien, de var die doorgegoven word in 'onchange' is de id
+                 var inh=xmlhttp.responseXML.getElementsByTagName('Onderwerp')[i].firstChild.data;
+                 var id=xmlhttp.responseXML.getElementsByTagName('Id')[i].firstChild.data;
+                 form.onderwerp.options.add( new Option(inh,id));
+}
+}
+}
