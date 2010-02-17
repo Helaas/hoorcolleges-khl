08f8bdc4d7d59cd2a72fba849ba3c296f6b83a0c
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 10:38:29 +0000

    git-svn-id: svn://localhost/repo@67 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/student.php
+++ trunk/Hoorcollege/student.php
@@ -10,16 +10,23 @@ if(isset ($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() == 1){
     $TBS->LoadTemplate('./html/student/templateStudent.html') ;
     $gebruikerID = $_SESSION['gebruiker']->getIdGebruiker();
 
-    //Query voor overzicht hoorcolleges
-    $query = 'SELECT * FROM hoorcollege_hoorcollege WHERE idHoorcollege IN (
-              SELECT Hoorcollege_idHoorcollege FROM hoorcollege_onderwerphoorcollege WHERE Onderwerp_Vak_idVak IN(
-              SELECT Vak_idVak FROM hoorcollege_gebruiker_volgt_vak WHERE Gebruiker_idGebruiker = '.$gebruikerID.' ))';
+    //Query voor overzicht hoorcolleges nog te bekijken
+    $queryNogTeBekijken = 'SELECT idHoorcollege, naam, vbc
+                          FROM hoorcollege_hoorcollege LEFT OUTER JOIN hoorcollege_gebruikerhoorcollege
+                          ON Hoorcollege_idHoorcollege = idHoorcollege
+                          WHERE Gebruiker_idGebruiker = '.$gebruikerID.' AND reedsBekeken = false';
+
+    //Query voor overzicht hoorcolleges reeds bekeken
+    $queryReedsBekeken = 'SELECT idHoorcollege, naam, vbc
+                         FROM hoorcollege_hoorcollege LEFT OUTER JOIN hoorcollege_gebruikerhoorcollege
+                         ON Hoorcollege_idHoorcollege = idHoorcollege
+                         WHERE Gebruiker_idGebruiker = '.$gebruikerID.' AND reedsBekeken = true';
 
     //Tabel nog te bekijken hoorcolleges opvullen (nog aan te passen)
-    $TBS->MergeBlock('blk1', $db, $query);
+    $TBS->MergeBlock('blk1', $db, $queryNogTeBekijken);
 
     //Tabel reeds bekeken hoorcolleges opvullen (nog aan te passen)
-    $TBS->MergeBlock('blk2', $db, $query);
+    $TBS->MergeBlock('blk2', $db, $queryReedsBekeken);
 
 }else if(!isset ($_SESSION['gebruiker'])){
     header("location: login.php");
