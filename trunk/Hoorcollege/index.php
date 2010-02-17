7fb419233e0efed7f78749fa1907a523494db19e
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 15:55:13 +0000

    typfout
    git-svn-id: svn://localhost/repo@82 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/index.php
+++ trunk/Hoorcollege/index.php
@@ -9,7 +9,7 @@ $config["pagina"] = "index.html";
 if(isset($_SESSION['gebruiker'])){
     if($_SESSION['gebruiker']->getNiveau() == 1){
         $TBS->LoadTemplate('./html/student/templateStudent.html');
-    }elseif($_SESSION['gebruiker']->getNiveau() == 40){
+    }else if($_SESSION['gebruiker']->getNiveau() == 40){
         $TBS->LoadTemplate('./html/lector/templateLector.html');
     }else if($_SESSION['gebruiker']->getNiveau() == 99){
         $TBS->LoadTemplate('./html/admin/templateAdmin.html');
