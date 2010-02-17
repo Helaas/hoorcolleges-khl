2b83c8fd45f440b49a5f43e69b016fd65b1edcb6
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 18:12:58 +0000

    git-svn-id: svn://localhost/repo@87 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/hoorcollegeVragenBeantwoorden.php
+++ trunk/Hoorcollege/hoorcollegeVragenBeantwoorden.php
@@ -1,6 +1,32 @@
 <?php
-include_once('./includes/kern.php');
+    include_once('./includes/kern.php');
 
- if (heeftHoorcollegeVragen(1)) echo "ja";
+ //if (heeftHoorcollegeVragen(1))
+
+    $config["pagina"] = "hoorcollegeVragenBeantwoorden.html";
+
+    $vragen = array();
+     $resultaat = $db->Execute('SELECT * FROM hoorcollege_vraag');
+     while (!$resultaat->EOF) {
+        $vragen[$resultaat->fields["idVraag"]]["vraagstelling"] =  $resultaat->fields["vraagstelling"];
+        $resultaat->MoveNext();
+    }
+
+
+    $vragen[1]["mogelijkantwoorden"][] = "mogelijkantwoord1";
+    $vragen[1]["mogelijkantwoorden"][] = "mogelijkantwoord2";
+
+        $vragen[2]["mogelijkantwoorden"][] = "mogelijkantwoord1";
+    $vragen[2]["mogelijkantwoorden"][] = "mogelijkantwoord2";
+   ?>
+    <pre>
+<?php print_r($vragen); ?>
+    </pre>
+<?php
+    $TBS->LoadTemplate('./html/template.html');
+    $TBS->MergeBlock("blk1",$vragen);
+    $TBS->Show();
+
+ //hoorcollegeVragenBeantwoorden
 
 ?>
