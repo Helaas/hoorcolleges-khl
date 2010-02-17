ef6e5511b82c75af9f450bd120a319f3f9c6cada
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 10:55:20 +0000

    templateStudent
    git-svn-id: svn://localhost/repo@70 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/index.php
+++ trunk/Hoorcollege/index.php
@@ -1,10 +1,20 @@
 <?php
-session_start();
 include_once('./includes/kern.php');
+session_start();
 
+$TBS = new clsTinyButStrong;
 $config["pagina"] = "index.html";
 
-$TBS->LoadTemplate('./html/template.html') ;
+
+if(isset($_SESSION['gebruiker'])){
+    if($_SESSION['gebruiker']->getNiveau() == 1){
+        $TBS->LoadTemplate('./html/student/templateStudent.html');
+    }else if($_SESSION['gebruiker']->getNiveau() == 40){
+        $TBS->LoadTemplate('./html/lector/templateLector.html');
+    }
+}else{
+    $TBS->LoadTemplate('./html/template.html') ;
+}
 
 
 $TBS->Show() ;
