7471a7ddea3ff68be7aed0225683ab1e4c88a4f6
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 15:53:06 +0000

    Lege index voor super admins fix-fix
    git-svn-id: svn://localhost/repo@81 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/index.php
+++ trunk/Hoorcollege/index.php
@@ -11,12 +11,10 @@ if(isset($_SESSION['gebruiker'])){
         $TBS->LoadTemplate('./html/student/templateStudent.html');
     }elseif($_SESSION['gebruiker']->getNiveau() == 40){
         $TBS->LoadTemplate('./html/lector/templateLector.html');
+    }else if($_SESSION['gebruiker']->getNiveau() == 99){
+        $TBS->LoadTemplate('./html/admin/templateAdmin.html');
     }
-    else {
-        $TBS->LoadTemplate('./html/template.html');
-        //Hier moet ergens nog rekening gehouden worden met userlevel 99 enzo
-        //Slordige code jongens
-    }
+
 }else{
     $TBS->LoadTemplate('./html/template.html') ;
 }
