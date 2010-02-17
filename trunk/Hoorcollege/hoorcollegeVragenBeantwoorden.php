66094c578335d6c72ebca603a386b54f4588f0d6
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-17 18:40:55 +0000

    Vragen beantwoorden begint vorm te krijgen
    git-svn-id: svn://localhost/repo@90 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/hoorcollegeVragenBeantwoorden.php
+++ trunk/Hoorcollege/hoorcollegeVragenBeantwoorden.php
@@ -9,15 +9,23 @@
      $resultaat = $db->Execute('SELECT * FROM hoorcollege_vraag');
      while (!$resultaat->EOF) {
         $vragen[$resultaat->fields["idVraag"]]["vraagstelling"] =  $resultaat->fields["vraagstelling"];
+        $vragen[$resultaat->fields["idVraag"]]["id"] =  $resultaat->fields["idVraag"];
         $resultaat->MoveNext();
     }
 
+     $resultaat = $db->Execute('SELECT * FROM hoorcollege_mogelijkantwoord');
+     while (!$resultaat->EOF) {
+        $vragen[$resultaat->fields["Vraag_idVraag"]]["mogelijkantwoorden"][] = array ("antwoord" => $resultaat->fields["antwoord"],
+           "id" => $resultaat->fields["idMogelijkAntwoord"] );
+        $resultaat->MoveNext();
+    }
 
+/**
     $vragen[1]["mogelijkantwoorden"][] = "mogelijkantwoord1";
     $vragen[1]["mogelijkantwoorden"][] = "mogelijkantwoord2";
 
         $vragen[2]["mogelijkantwoorden"][] = "mogelijkantwoord1";
-    $vragen[2]["mogelijkantwoorden"][] = "mogelijkantwoord2";
+    $vragen[2]["mogelijkantwoorden"][] = "mogelijkantwoord2";**/
    ?>
     <pre>
 <?php print_r($vragen); ?>
