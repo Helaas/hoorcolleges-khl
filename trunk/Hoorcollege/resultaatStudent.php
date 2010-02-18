ac537407bdbc52a584c7d33367e7c6f596c7d19d
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-18 16:03:06 +0000

    Overzicht vragen student - nog aan te passen
    git-svn-id: svn://localhost/repo@96 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/resultaatStudent.php
+++ trunk/Hoorcollege/resultaatStudent.php
@@ -8,14 +8,35 @@
         $config["pagina"] = "./student/resultaatStudent.html";
         $TBS->LoadTemplate('./html/student/templateStudent.html');
         $gebruikerID = $_SESSION['gebruiker']->getIdGebruiker();
-        $hoorcollegeID = $_GET['hoorcollege'];
+        $resultaat = $db->GetRow('select * from hoorcollege_hoorcollege where idHoorcollege='.$_GET['hoorcollege']);
+        $naamHoorcollege = $resultaat['naam'];
 
-        echo "Is het antwoord juist? ".antwoordOk($gebruikerID, 2);
+        $vragen = array();
+        $idVraag;
+        $resultaat = $db->Execute('SELECT * FROM hoorcollege_vraag WHERE Hoorcollege_idHoorcollege = '.$_GET['hoorcollege']);
+        while (!$resultaat->EOF) {
+                $idVraag = $resultaat->fields["idVraag"];
+                $vragen[$resultaat->fields[$idVraag]]["vraagstelling"] =  $resultaat->fields["vraagstelling"];
+                $vragen[$resultaat->fields[$idVraag]]["id"] =  $resultaat->fields["idVraag"];
+                $vragen[$resultaat->fields[$idVraag]]["juistAntwoord"] =  $resultaat->fields["juistantwoord"];
+                $resultaat->MoveNext();
+            }
+
+        $resultaat2 = $db->Execute('SELECT * FROM hoorcollege_gegevenantwoord WHERE Vraag_idVraag = '.$idVraag.'
+             AND Gebruiker_idGebruiker = '.$gebruikerID);
+
+            $gegevenAntwoord = $resultaat2->fields["MogelijkAntwoord_idMogelijkAntwoord"];
+            $antwoordJuist = antwoordOk($gebruikerID, $idVraag);
+            $vragen[$resultaat->fields[$idVraag]]["gegevenAntwoord"] = $gegevenAntwoord;
+            $vragen[$resultaat->fields[$idVraag]]["juist"] = $antwoordJuist;
+
+        $TBS->MergeBlock("blk1",$vragen);
 
     }else if(!isset ($_SESSION['gebruiker'])){
         header("location: login.php");
     }else{
         $config["pagina"] = "./FileUpload/Error1Login.html";
+        $TBS->LoadTemplate('./html/template.html') ;
     }
 
     $TBS->Show();
