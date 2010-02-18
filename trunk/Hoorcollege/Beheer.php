48e5a21821bf39c1fd13b4a66751ce183fe8bcc8
Fenshyr <Fenshyr@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-18 12:31:10 +0000

    Dynamische creatie van de onderwerp select en overzicht tabel, begin van Beheer.html voor het toevoegen van onderwerpen/hoorcolleges
    git-svn-id: svn://localhost/repo@94 10651035-e252-4840-a10e-5981b4cf34d1

---
+++ trunk/Hoorcollege/Beheer.php
@@ -0,0 +1,33 @@
+<?php
+    include_once('./includes/kern.php');
+    $config["pagina"] = "lector/Beheer.html";
+
+    session_start();
+
+if(isset ($_SESSION['gebruiker'])) {
+        $gebruiker = $_SESSION['gebruiker'];
+        $gebruikerNiv = $gebruiker->getNiveau();
+
+        if($gebruikerNiv==40){
+    $q = "select * from hoorcollege_vak where idVak in(SELECT Vak_idVak FROM `hoorcollege_gebruiker_beheert_vak` WHERE gebruiker_idgebruiker =".$_SESSION['gebruiker']->getIdGebruiker().")";
+    $TBS->LoadTemplate('./html/Lector/templateLector.html');
+    $TBS->MergeBlock("blk1",$db,$q);
+    $TBS->Show();
+        }
+                  else {
+        $config["pagina"] = "./FileUpload/Error1Login.html";
+         $TBS->LoadTemplate('./html/template.html') ;
+        $TBS->Show() ;
+
+    }
+}
+            else {
+        $config["pagina"] = "./FileUpload/Error1Login.html";
+         $TBS->LoadTemplate('./html/template.html') ;
+        $TBS->Show() ;
+
+    }
+
+
+
+?>
--- trunk/Hoorcollege/html/lector/Functies.js
+++ trunk/Hoorcollege/html/lector/Functies.js
@@ -34,32 +34,109 @@ function autoSubmit(form)
   xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vakid, false);
   xmlhttp.send(null);
 
- //alert(xmlhttp.responseXML);
+
               
                 //haal optie uit de XML en voeg toe aan dropdown
                 var elems = xmlhttp.responseXML.getElementsByTagName("Onderwerp");
  		var size = elems.length;
-                while(form.onderwerp.options.length>1){
-                form.onderwerp.options[1]=null;
+
+                //div dynamisch opvullen met een select gebaseerd op het gekozen vak
+                document.getElementById("kiesond").innerHTML='Kies een onderwerp:';
+                 var brk=document.createElement('br');
+                 document.getElementById("kiesond").appendChild(brk);
+                 var sel=document.createElement('select');
+                 sel.name='Ond';
+                 sel.onchange=function(){autoSubmit2(this.form);};
+                 var opt1= document.createElement("option");
+                 opt1.text='--Selecteer een onderwerp--';
+                 opt1.value=0;
+                 sel.options.add(opt1);
+                 document.getElementById("kiesond").appendChild(sel);
+                while(sel.options.length>1){
+                sel.options[1]=null;
                 }
                 for(i = 0; i < size; i++){
                  //onderwerp = naam van het onderwerp, id = id van het onderwerp, gebruiker krijgt de naam te zien, de var die doorgegoven word in 'onchange' is de id
                  var inh=xmlhttp.responseXML.getElementsByTagName('Onderwerp')[i].firstChild.data;
                  var id=xmlhttp.responseXML.getElementsByTagName('Id')[i].firstChild.data;
                  
-                 form.onderwerp.options.add( new Option(inh,id));
-
-
-
+                 sel.options.add( new Option(inh,id));
+                }
 
 }
+
+function autoSubmitBeheer(form)
+{
+
+
+  var vakid=form.vak.options[form.vak.options.selectedIndex].value;
+
+
+      var xmlhttp;
+     try {
+    // Mozilla / Safari / IE7
+         xmlhttp = new XMLHttpRequest();
+     } catch (e) {
+          // IE
+           var XMLHTTP_IDS = new Array('MSXML2.XMLHTTP.5.0',
+                                    'MSXML2.XMLHTTP.4.0',
+                                    'MSXML2.XMLHTTP.3.0',
+                                    'MSXML2.XMLHTTP',
+                                    'Microsoft.XMLHTTP' );
+         var success = false;
+        for (var i=0;i < XMLHTTP_IDS.length && !success; i++) {
+             try {
+                  xmlhttp = new ActiveXObject(XMLHTTP_IDS[i]);
+                    success = true;
+               } catch (e) {}
+         }
+        if (!success) {
+             throw new Error('Unable to create XMLHttpRequest.');
+         }
+    }
+
+
+  xmlhttp.onreadystatechange=Verwerk;
+  //xmlhttp request om via php een xml pagina aan te maken op basis van het meegegoven vakid
+  xmlhttp.open("GET", "VerwerkDropdown.php?gevraagdVak="+vakid, false);
+  xmlhttp.send(null);
+
+
+
+                //haal optie uit de XML en voeg toe aan dropdown
+                var elems = xmlhttp.responseXML.getElementsByTagName("Onderwerp");
+ 		var size = elems.length;
+
+                //div dynamisch opvullen met een select gebaseerd op het gekozen vak
+                document.getElementById("kiesond").innerHTML='Kies een onderwerp:';
+                 var brk=document.createElement('br');
+                 document.getElementById("kiesond").appendChild(brk);
+                 var sel=document.createElement('select');
+                 sel.name='Ond';
+                 var opt1= document.createElement("option");
+                 opt1.text='--Selecteer een onderwerp--';
+                 opt1.value=0;
+                 sel.options.add(opt1);
+                 document.getElementById("kiesond").appendChild(sel);
+                while(sel.options.length>1){
+                sel.options[1]=null;
+                }
+                for(i = 0; i < size; i++){
+                 //onderwerp = naam van het onderwerp, id = id van het onderwerp, gebruiker krijgt de naam te zien, de var die doorgegoven word in 'onchange' is de id
+                 var inh=xmlhttp.responseXML.getElementsByTagName('Onderwerp')[i].firstChild.data;
+                 var id=xmlhttp.responseXML.getElementsByTagName('Id')[i].firstChild.data;
+
+                 sel.options.add( new Option(inh,id));
+                }
+
 }
+
 
 function autoSubmit2(form)
 {
 
      var vakid=form.vak.options[form.vak.options.selectedIndex].value;
-     var ondid=form.onderwerp.options[form.onderwerp.options.selectedIndex].value;
+     var ondid=form.Ond.options[form.Ond.options.selectedIndex].value;
 
 
 
@@ -93,27 +170,7 @@ function autoSubmit2(form)
   xmlhttp.open("GET", "VerwerkDropdown2.php?gevraagdVak="+vakid+"&gevraagdOnd="+ondid, false);
   xmlhttp.send(null);
 
-         
-              //Vul Tabel Op
-               //var t1=document.getElementById('t1');
-                //for (var g=0;g<size;g++){
-                //var table=document.createElement('TABLE');
-               //table.border='1';
-                 // var tbdy=document.createElement('TBODY');
-                //table.appendChild(tbdy);
-                //for (var j=0;j<2;j++){
-                //var tr=document.createElement('TR');
-                  //tbdy.appendChild(tr);
-                   //for (var k=0;k<2;k++){
-                   //var td=document.createElement('TD');
-                 //td.width='100';
-                //var inh=xmlhttp.responseXML.getElementsByTagName('Naam')[g].firstChild.data;
-                //td.appendChild(document.createTextNode(inh));
-                 //tr.appendChild(td);
-                     //}
-                       //}
-                       //t1.appendChild(table);
-                     //}
+ 
 
                      
                 //Tabel aanmaken
