6d0fe2ac655d276204898a1092c844de618d7fb3
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-23 09:36:05 +0000

    Fenshyr - SQL Injection fix
    git-svn-id: svn://localhost/repo@150 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/OverzichtHoorcolleges.php
+++ trunk/Hoorcollege/OverzichtHoorcolleges.php
@@ -10,11 +10,13 @@ if(isset ($_SESSION['gebruiker'])) {
         $gebruikerNiv = $gebruiker->getNiveau();
 
         if($gebruikerNiv==40){
+
+          if (preg_match('/^[0-9]+$/iD', $_SESSION['gebruiker']->getIdGebruiker())) {
     $q = "select * from hoorcollege_vak where idVak in(SELECT Vak_idVak FROM `hoorcollege_gebruiker_beheert_vak` WHERE gebruiker_idgebruiker =".$_SESSION['gebruiker']->getIdGebruiker().")";
     $TBS->LoadTemplate('./html/lector/templateLector.html');
     $TBS->MergeBlock("blk1",$db,$q);
     $TBS->Show();
-        }
+        }}
                   else {
         $config["pagina"] = "./FileUpload/Error1Login.html";
          $TBS->LoadTemplate('./html/template.html') ;
