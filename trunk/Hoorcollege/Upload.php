052f30fd2a4badb70c2254a2ea93420f5b18e583
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-23 15:13:02 +0000

    Fenshyr Uploaden: enkell mp3+FLV uploaden, max file size= 1 gb, moet aangepast worden in wamp's ini.php(filesize=1024MB), file krijgt een naam van de user, maar word opgeslagen dmv een willekeurig nummer tss 1 en 1000000.
    git-svn-id: svn://localhost/repo@165 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/Upload.php
+++ trunk/Hoorcollege/Upload.php
@@ -2,6 +2,8 @@
 <?php
 include_once('./includes/TinyButStrong.php');
 include_once('./includes/gebruiker.class.php');
+include_once('./includes/kern.php');
+
 $TBS = new clsTinyButStrong;
 
 session_start();
@@ -20,7 +22,13 @@ if(isset ($_SESSION['gebruiker'])) {
 
         if($gebruikerNiv==40 ){
             //Folder mag geen speciale tekens zoals een punt bevatten, anders zou een vak als .NET bvb een hidden folder aanmaken
-         if (preg_match('/^[a-z0-9\+\#\ ]+$/iD', $_POST['cat']) && preg_match('/^[0-9]+$/iD', $_SESSION['gebruiker']->getIdGebruiker()) && preg_match('/^[a-z0-9\+\#\ ]+$/iD', $_POST['bestandsnaam'])) {
+         if (preg_match('/^[a-z0-9\+\#\ ]+$/iD', $_POST['cat']) && preg_match('/^[0-9]+$/iD', $gebruikerID) && preg_match('/^[a-z0-9\+\#\ ]+$/iD', $_POST['bestandsnaam'])) {
+           
+           $stringArray = preg_split('/\./',$_FILES['uploadedfile']['name']);
+
+      
+      if(strtolower($stringArray[count($stringArray)-1])== 'mp3' || strtolower($stringArray[count($stringArray)-1])== 'flv'){
+
 
     //Kijk of de folder reeds bestaat, zoniet maak hem aan, herhaal dit voor alle subfolders
     if (!is_dir("Bibliotheek/".$gebruikerID."/")){
@@ -30,19 +38,24 @@ if(isset ($_SESSION['gebruiker'])) {
     if (!is_dir("Bibliotheek/".$gebruikerID."/".$_POST['cat']."/")){
         mkdir("Bibliotheek/".$gebruikerID."/".$_POST['cat'], 0777);
     }
-
+$get= rand(1,1000000);
 
 //Path waar het bestand heen moet
 $target_path = "Bibliotheek/".$gebruikerID."/".$_POST['cat']."/";
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+$target_path = $target_path .$get.'.'.$stringArray[count($stringArray)-1];
+while(file_exists($target_path)){$get= rand(1,1000000);$target_path = "Bibliotheek/".$gebruikerID."/".$_POST['cat']."/".$get.'.'.$stringArray[count($stringArray)-1];}
 
 //Code voor het verplaatsen/kopiëren van het tijdelijke bestand naar de server+pagina weergeven
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
    $testinhoud="Het bestand ".'"'.  basename( $_FILES['uploadedfile']['name']).'"'." werd geüpload!";
+   $db->Execute("INSERT INTO hoorcollege_bibliotheekitem (mimetype, beschrijving, locatie, tekst, naam, BibliotheekCategorie_idBibliotheekCategorie) VALUES ('".$stringArray[count($stringArray)-1]."', 'test', '".$target_path."', 'test', '".$_POST['bestandsnaam']."','".$_POST['cat']."')");
 } else{
     $testinhoud="Er was een probleem bij het uploaden van je bestand, probeer het later opnieuw!  (De maximale bestandsgrootte is ".$_POST['MAX_FILE_SIZE']." bytes)";
 }
-
+}
+else{
+    $testinhoud="U kan momenteel enkel bestanden met extensie .mp3 of .flv uploaden.";
+}
 
 $TBS->LoadTemplate('./html/lector/templateLector.html') ;
 $TBS->Show() ;
