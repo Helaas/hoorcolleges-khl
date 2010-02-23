24d7e4a4ad21b863b9ca8d20cba553d75d6f49a9
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-23 13:36:42 +0000

    Hoorcolleges weergeven aanzetje
    git-svn-id: svn://localhost/repo@162 10651035-e252-4840-a10e-5981b4cf34d1

---
+++ trunk/Hoorcollege/hoorcollege.php
@@ -0,0 +1,26 @@
+<?php
+// && heeftHoorcollegeVragen($_GET["hoorcollege"]) && magGebruikerVragenBeantwoorden($_SESSION['gebruiker']->getIdGebruiker(),$_GET["hoorcollege"])
+    require("./includes/kern.php");
+    session_start();
+
+    if(isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() == 1){ //student is ingelogged
+
+        if (isset($_GET["hoorcollege"]) && is_numeric($_GET["hoorcollege"]) && magGebruikerHoorcollegeZien($_SESSION['gebruiker']->getIdGebruiker(),$_GET["hoorcollege"])){
+            $config["pagina"] = "./hoorcollege/temp.html";
+            $TBS->LoadTemplate('./html/student/templateStudent.html');
+        } else {
+            $fout["reden"] = "Geen hoorcollege beschikbaar";
+            $fout["inhoud"] = "U beschikt over onvoldoende rechten om dit hoorcollege te bekijken.";
+            $config["pagina"] = "./algemeneFout.html";
+            $TBS->LoadTemplate('./html/student/templateStudent.html');
+        }
+
+    } else { //geen student / niet ingelogged
+        $config["pagina"] = "./FileUpload/Error1Login.html";
+        $TBS->LoadTemplate('./html/template.html');
+    }
+
+    $TBS->Show();
+
+
+?>
