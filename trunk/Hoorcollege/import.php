84aa1bf9a2dd3dca8dfd14e3e11d8cc3fb5494ea
aerts.birgit <aerts.birgit@10651035-e252-4840-a10e-5981b4cf34d1>
2010-02-26 22:11:37 +0000

    Probleem import hopelijk opgelost voor Windowsgebruikers
    git-svn-id: svn://localhost/repo@224 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/import.php
+++ trunk/Hoorcollege/import.php
@@ -18,40 +18,42 @@ function add_data( $groep, $gebruikerId) {
 /*
  * Wanneer er een file wordt toegevoegd
 */
-if ($_FILES['file']['tmp_name'] ) {
-    $dom = DOMDocument::load( $_FILES['file']['tmp_name'] );
-    $rows = $dom->getElementsByTagName('Row');
-    $first_row = true;  //Zodat eerste row als header wordt gezien
-    foreach ($rows as $row) {
-        if ( !$first_row ) {
-            $groep = "";
-            $gebruikerId = "";
-            $index = 1;
-            $cells = $row->getElementsByTagName( 'Cell' );
-            foreach( $cells as $cell ) {
-                $ind = $cell->getAttribute( 'ss:Index' );
-                if ( $ind != null ) $index = $ind;
-                if ( $index == 1 ) $groep = $cell->nodeValue;
-                if ( $index == 2 ) $gebruikerId = $cell->nodeValue;
-                $index += 1;
+if (isset($_POST["fileUploaden"])) {
+    if ($_FILES['file']['tmp_name'] ) {
+        $dom = DOMDocument::load( $_FILES['file']['tmp_name'] );
+        $rows = $dom->getElementsByTagName('Row');
+        $first_row = true;  //Zodat eerste row als header wordt gezien
+        foreach ($rows as $row) {
+            if ( !$first_row ) {
+                $groep = "";
+                $gebruikerId = "";
+                $index = 1;
+                $cells = $row->getElementsByTagName( 'Cell' );
+                foreach( $cells as $cell ) {
+                    $ind = $cell->getAttribute( 'ss:Index' );
+                    if ( $ind != null ) $index = $ind;
+                    if ( $index == 1 ) $groep = $cell->nodeValue;
+                    if ( $index == 2 ) $gebruikerId = $cell->nodeValue;
+                    $index += 1;
+                }
+                add_data( $groep, $gebruikerId);
             }
-            add_data( $groep, $gebruikerId);
+            $first_row = false;
         }
-        $first_row = false;
-    }
-    /*
+        /*
      * Voor elke rij in de file
-    */
-    foreach( $data as $row ) {
-        $gebruikerId = $row['gebruikerId'];
-        if(validateNumber($gebruikerId) && strlen($row['groep']) < 8) {
-            if(!bestaatGroep($row['groep'])) {
-                voegGroepToe($groep);
-            }
-            $groepId = getGroepId($row['groep']);
-            //Ervoor zorgen dat een student geen 2keer aan dezelfde groep wordt toegevoegd
-            if(!isStudentToegekentAanGroep2($gebruikerId, $groepId)) {
-                kenStudentToeAanGroep($gebruikerId, $groepId);
+        */
+        foreach( $data as $row ) {
+            $gebruikerId = $row['gebruikerId'];
+            if(validateNumber($gebruikerId) && strlen($row['groep']) < 8) {
+                if(!bestaatGroep($row['groep'])) {
+                    voegGroepToe($groep);
+                }
+                $groepId = getGroepId($row['groep']);
+                //Ervoor zorgen dat een student geen 2keer aan dezelfde groep wordt toegevoegd
+                if(!isStudentToegekentAanGroep2($gebruikerId, $groepId)) {
+                    kenStudentToeAanGroep($gebruikerId, $groepId);
+                }
             }
         }
     }
