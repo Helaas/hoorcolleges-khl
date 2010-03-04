0424a3dc6b849dd2bb9aca42b3bbd17882f947fe
beslic.filip <beslic.filip@10651035-e252-4840-a10e-5981b4cf34d1>
2010-03-04 20:19:49 +0000

    Studenten aan groep koppelen gewijzigd, beter gemaakt :)
    git-svn-id: svn://localhost/repo@330 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/admin.php
+++ trunk/Hoorcollege/admin.php
@@ -1025,154 +1025,30 @@ if(isset ($_GET['pagina'])) {
         //dropdown voor alle vakken
         $TBS->MergeBlock('blk2', $db, 'SELECT * FROM hoorcollege_groep GROUP BY naam asc');
 
-        $filteroptiesNaam = false;
-        $filteroptiesVak = false;
-        $filteroptiesGroep = false;
-
-        if(isset ($_POST['studentGroepVerder'])) { //nagaan of men op deze pagina wel via correcte weg is gekomen
-            //aantal controles uitvoeren:
-            if(isset ($_POST['filteroptiesNaam']) && !empty ($_POST['naamBegintMet'])) { //nagaan of filteroptiesnaam is ingevuld en deze niet leeg is, indien dat wel is wordt dat genegeerd
-                $filteroptiesNaam = true;
-            }
-            if(isset ($_POST['filteroptiesVak']) && $_POST['naamBegintMet'] != 'leeg') { //nagaan op de filteroptiesVak zijn aangevinkt en of men effectief een vak heeft gekozen, anders wordt dit genegeerd
-                $filteroptiesVak = true;
-            }
-            if(isset ($_POST['filteroptiesGroep'])) {
-                $filteroptiesGroep = true;
-            }
-
-            //verwerking:
-            //indien alle 3 opties zijn gekozen
-            if($filteroptiesNaam && $filteroptiesVak && $filteroptiesGroep) { //indien alle opties zijn gekozen en alles correct is geselcteerd/ingevuld
-                $naamBegintMet = (string) $_POST['naamBegintMet'];
-                $vak = (int) $_POST['selectVak'];
-                $groep = (int) $_POST['selectGroep'];
-                if($groep != 'zonder') {  //indien men een groep heeft geselecteerd
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                            FROM hoorcollege_gebruiker g
-                                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                                            WHERE g.niveau = '1' and g.actief = '1' AND vv.Vak_idVak = '$vak' AND g.naam LIKE '%$naamBegintMet%'
-                                                            AND g.idGebruiker
-                                                            IN (SELECT Gebruiker_idGebruiker
-                                                                FROM hoorcollege_gebruikergroep
-                                                                WHERE Groep_idGroep = '$groep')
-                                                            GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-                else { //indien men bij groep de optie zonder groep heeft genomen
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                            FROM hoorcollege_gebruiker g
-                                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                                            WHERE g.niveau = '1' and g.actief = '1' AND vv.Vak_idVak = '$vak' AND g.naam LIKE '%$naamBegintMet%'
-                                                            AND g.idGebruiker
-                                                            NOT IN (SELECT Gebruiker_idGebruiker
-                                                                FROM hoorcollege_gebruikergroep)
-                                                            GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-            }
-            else if($filteroptiesNaam && $filteroptiesVak && !$filteroptiesGroep) { //indien men enkel de 2 bovenste veldjes heeft aangevinkt
-                $naamBegintMet = (string) $_POST['naamBegintMet'];
-                $vak = (int) $_POST['selectVak'];
-                $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                            FROM hoorcollege_gebruiker g
-                                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                                            WHERE g.niveau = '1' and g.actief = '1' AND vv.Vak_idVak = '$vak' AND g.naam LIKE '%$naamBegintMet%'
-                                                            GROUP BY g.naam, g.voornaam ASC");
-                $studdata = serialize($data);
-            }
-            else if(!$filteroptiesNaam && $filteroptiesVak && $filteroptiesGroep) { //indien men enkel de 2 onderste veldjes heeft aangevinkt
-                $vak = (int) $_POST['selectVak'];
-                $groep = (int) $_POST['selectGroep'];
-                if($groep != 'zonder') {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                            FROM hoorcollege_gebruiker g
-                                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                                            WHERE g.niveau = '1' and g.actief = '1' AND vv.Vak_idVak = '$vak' AND g.niveau = '1'
-                                                            AND g.idGebruiker
-                                                            IN (SELECT Gebruiker_idGebruiker
-                                                                FROM hoorcollege_gebruikergroep
-                                                                WHERE Groep_idGroep = '$groep')
-                                                            GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-                else {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                            FROM hoorcollege_gebruiker g
-                                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                                            WHERE g.niveau = '1' and g.actief = '1' AND vv.Vak_idVak = '$vak' AND g.niveau = '1'
-                                                            AND g.idGebruiker
-                                                            NOT IN (SELECT Gebruiker_idGebruiker
-                                                                FROM hoorcollege_gebruikergroep)
-                                                            GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-            }
-            else if($filteroptiesNaam && !$filteroptiesVak && $filteroptiesGroep) { //indien men de bovenste en onderste veldjes heeft aangevinkt
-                $naamBegintMet = (string) $_POST['naamBegintMet'];
-                $groep = (int) $_POST['selectGroep'];
-                if($groep != 'zonder') {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                           FROM hoorcollege_gebruiker g
-                                                           LEFT JOIN hoorcollege_gebruikergroep gg ON g.idGebruiker = gg.Gebruiker_idGebruiker
-                                                           WHERE gg.Groep_idGroep = '$groep' AND g.naam LIKE '%$naamBegintMet%' AND g.niveau = '1'");
-                    $studdata = serialize($data);
-                }
-                else {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                           FROM hoorcollege_gebruiker g
-                                                           WHERE g.naam LIKE '%$naamBegintMet%' AND g.niveau = '1' and g.actief = '1' AND g.idGebruiker
-                                                           NOT IN (SELECT Gebruiker_idGebruiker
-                                                                FROM hoorcollege_gebruikergroep)
-                                                           GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-            }
-            else if($filteroptiesNaam) { //indien enkel de eerste is geselcteerd
-                $naamBegintMet = (string) $_POST['naamBegintMet'];
-                $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                                       FROM hoorcollege_gebruiker g
-                                                       WHERE g.naam LIKE '%$naamBegintMet%' AND g.niveau = '1' and g.actief = '1'");
-                $studdata = serialize($data);
-            }
-            else if($filteroptiesVak) { //indien enkel de tweede is geselcteerd
-                $vak = (int) $_POST['selectVak'];
-                $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                            FROM hoorcollege_gebruiker g
-                                            LEFT JOIN hoorcollege_gebruiker_volgt_vak vv ON g.idGebruiker = vv.Gebruiker_idGebruiker
-                                            WHERE vv.Vak_idVak = '$vak'
-                                            GROUP BY g.naam, g.voornaam ASC");
-                $studdata = serialize($data);
-            }
-            else if($filteroptiesGroep) { //indien enkel het laatste veldje is geselcteerd
-                $groep = (int) $_POST['selectGroep'];
-                if($groep != 'zonder') {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
-                                              FROM hoorcollege_gebruiker g
-                                              LEFT JOIN hoorcollege_gebruikergroep gr ON g.idGebruiker = gr.Gebruiker_idGebruiker
-                                              WHERE gr.Groep_idGroep = '$groep' AND g.niveau = '1' and g.actief = '1'
-                                              GROUP BY g.naam, g.voornaam ASC");
-                    $studdata = serialize($data);
-                }
-                else {
-                    $data = $TBS->MergeBlock('blk1,*', $db, "SELECT * FROM hoorcollege_gebruiker
-                                              WHERE niveau = 1 and actief = 1 AND idGebruiker NOT IN
-                                              (SELECT Gebruiker_idGebruiker FROM hoorcollege_gebruikergroep)");
-                    $studdata = serialize($data);
-                }
-            }
+        if($_POST['groepFilter'] == 'alle') {
+            $studdata = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
+                                                     FROM hoorcollege_gebruiker g
+                                                     WHERE g.niveau = '1' and g.actief = '1' AND g.idGebruiker
+                                                     NOT IN (SELECT Gebruiker_idGebruiker
+                                                             FROM hoorcollege_gebruikergroep)
+                                                     GROUP BY g.naam, g.voornaam ASC");
         }
-        else {  //beveiliging
-            if(!isset ($_GET['foutboodschap'])) {
-                header("location: index.php");
-            }
+        else if($_POST['groepFilter'] == 'naam') {
+            $naamBegintMet = (string) $_POST['naamBegintMet'];
+            $studdata = $TBS->MergeBlock('blk1,*', $db, "SELECT g.idGebruiker, g.naam, g.voornaam
+                                                     FROM hoorcollege_gebruiker g
+                                                     WHERE g.naam LIKE '%$naamBegintMet%' AND g.niveau = '1' and g.actief = '1' AND g.idGebruiker
+                                                     NOT IN (SELECT Gebruiker_idGebruiker
+                                                             FROM hoorcollege_gebruikergroep)
+                                                     GROUP BY g.naam, g.voornaam ASC");
+            $studdata = serialize($studdata);
+        }
+        else {
+            header("location: index.php");
         }
     }
     else if($_GET['pagina'] == 'studentGroep') {
-        //dropdown voor alle vakken
-        $TBS->MergeBlock('blk1', $db, 'SELECT * FROM hoorcollege_vak GROUP BY naam asc');
-        //dropdown opvullen voor filter selectie klas
-        $TBS->MergeBlock('blk2', $db, 'SELECT * FROM hoorcollege_groep GROUP BY naam asc');
+        
     }
     else if($_GET['pagina'] == 'studentGroepVerderOverzicht') {
         $count = count($_POST['checkboxStudenten']);
