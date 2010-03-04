0077db9b52896f36232eea6ef0f6686f53644744
kevinvranken <kevinvranken@10651035-e252-4840-a10e-5981b4cf34d1>
2010-03-04 17:23:50 +0000

    Stukje code birgit beveiligd
    git-svn-id: svn://localhost/repo@326 10651035-e252-4840-a10e-5981b4cf34d1

--- trunk/Hoorcollege/lector/pasBibliotheekItemsAan.php
+++ trunk/Hoorcollege/lector/pasBibliotheekItemsAan.php
@@ -1,8 +1,14 @@
 <?php
     require_once('./../includes/kern.php');
     session_start();
-    $config["pagina"] = "./lector/pasBibliotheekItemsAan.html";
-    $TBS->LoadTemplate('./../html/lector/templateLector.html');
-    $TBS->Show();
+    if(isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() >= 40) { //lector is ingelogged
+        $config["pagina"] = "./lector/pasBibliotheekItemsAan.html";
+        $TBS->LoadTemplate('./../html/lector/templateLector.html');
+        $TBS->Show();
+    } else {
+        $config["pagina"] = "./FileUpload/Error1Login.html";
+        $TBS->LoadTemplate('./../html/lector/templateLector.html') ;
+        $TBS->Show() ;
+    }
 
 ?>
--- trunk/Hoorcollege/lector/verwerkCommentaarVerwijderen.php
+++ trunk/Hoorcollege/lector/verwerkCommentaarVerwijderen.php
@@ -1,11 +1,12 @@
 <?php
 include_once('./../includes/kern.php');
 session_start();
+if(isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']->getNiveau() >= 40) { //lector is ingelogged
+    $commentaarId = $_GET['id'];
+    echo $commentaarId;
 
-$commentaarId = $_GET['id'];
-echo $commentaarId;
-
-if(validateNumber($commentaarId)){
-    $db->Execute('DELETE FROM hoorcollege_reactie WHERE idReactie='.$commentaarId);
+    if(validateNumber($commentaarId)){
+        $db->Execute('DELETE FROM hoorcollege_reactie WHERE idReactie='.$commentaarId);
+    }
 }
 ?>
